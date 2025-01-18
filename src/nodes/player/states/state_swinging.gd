8a6c94b7fd058500440b18afd12fde61568261b8
zemda <dankilom26@gmail.com>
2025-01-18 15:56:31 +0100

    fix animation issue when swinging

--- src/nodes/player/states/state_swinging.gd
+++ src/nodes/player/states/state_swinging.gd
@@ -3,6 +3,9 @@ extends FSMState
 
 func update(delta: float) -> void:
 	host.apply_gravity(delta)
+	var input_axis = Input.get_axis("move_left", "move_right")
+	if input_axis:
+		host.animated_sprite_2d.flip_h = (input_axis < 0)
 	host.update_animations(0)
 	host.move_and_slide()
 	host.update_wall_state()
