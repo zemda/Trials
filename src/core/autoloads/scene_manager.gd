66dee26271df3cf581659fe496b078a5891b65ba
zemda <dankilom26@gmail.com>
2025-03-15 23:12:46 +0100

    add checkpoint scene

--- src/core/autoloads/scene_manager.gd
+++ src/core/autoloads/scene_manager.gd
@@ -3,6 +3,7 @@ extends Node
 const LoadingScreenPath = "res://ui/loading_screen/LoadingScreen.tscn"
 const PauseScreenPath = "res://ui/pause_screen/PauseScreen.tscn"
 const PlayerPath = "res://nodes/player/player.tscn"
+const BaseGameLevel = "res://levels/TestMainMap.tscn"
 
 
 func get_loading_screen_instance() -> Node:
