e24903946886497f1060d3603d9fa01e964635f8
zemda <dankilom26@gmail.com>
2025-03-15 19:20:01 +0100

    fix cyclic references

--- src/core/autoloads/scene_manager.gd
+++ src/core/autoloads/scene_manager.gd
@@ -1,3 +1,17 @@
 extends Node
 
-const LoadingScreenScene = preload("res://ui/loading_screen/LoadingScreen.tscn")
+const LoadingScreenPath = "res://ui/loading_screen/LoadingScreen.tscn"
+const PauseScreenPath = "res://ui/pause_screen/PauseScreen.tscn"
+const PlayerPath = "res://nodes/player/player.tscn"
+
+
+func get_loading_screen_instance() -> Node:
+	return load(LoadingScreenPath).instantiate()
+
+
+func get_pause_screen_instance() -> Node:
+	return load(PauseScreenPath).instantiate()
+
+
+func get_player_instance() -> Node:
+	return load(PlayerPath).instantiate()
