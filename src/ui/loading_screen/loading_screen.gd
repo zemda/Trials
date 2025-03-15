daaea3c5d49a56cc06ddc43f39652db8c683c98e
zemda <dankilom26@gmail.com>
2025-03-15 18:56:07 +0100

    add scene changer and loading screen

---
+++ src/ui/loading_screen/loading_screen.gd
@@ -0,0 +1,62 @@
+extends CanvasLayer
+
+var _progress_bar: ProgressBar
+var _loading_text: Label
+var _control_node: Control
+var _panel: Panel
+
+var _is_ready: bool = false
+
+func _ready():
+	process_mode = Node.PROCESS_MODE_ALWAYS
+	
+	_progress_bar = $Control/CenterContainer/VBoxContainer/ProgressBar
+	_loading_text = $Control/CenterContainer/VBoxContainer/LoadingText
+	_control_node = $Control
+	_panel = $Control/Panel
+	
+	_is_ready = true
+	
+	if _control_node.visible == true:
+		_progress_bar.value = 0
+		_loading_text.text = "LOADING..."
+	else:
+		_control_node.visible = false
+	
+	if SceneChanger.has_signal("progress_changed"):
+		SceneChanger.connect("progress_changed", Callable(self, "update_progress"))
+
+
+func show_loading_screen():
+	if !_is_ready:
+		return
+	
+	layer = 128
+	
+	_control_node.visible = true
+	_control_node.modulate.a = 1.0
+	_progress_bar.value = 0
+	
+	_loading_text.text = "LOADING..."
+
+
+func hide_loading_screen():
+	if !_is_ready:
+		return
+	
+	var tween = create_tween()
+	tween.tween_property(_control_node, "modulate:a", 0.0, 0.3)
+	await tween.finished
+	
+	_control_node.visible = false
+
+
+func update_progress(value: float):
+	if !_is_ready:
+		return
+		
+	var percent = value
+	if value <= 1.0:
+		percent = value * 100
+	
+	_progress_bar.value = percent
