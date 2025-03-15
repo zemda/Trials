056ca46efba29da9de1f8cf84e200b9836fd9f61
zemda <dankilom26@gmail.com>
2025-03-15 16:23:46 +0100

    fix cursor hotspot

--- src/core/autoloads/globals.gd
+++ src/core/autoloads/globals.gd
@@ -3,4 +3,5 @@ extends Node
 
 func _ready() -> void:
 	var mouse_arrow = preload("res://assets/cursor/4.png")
-	Input.set_custom_mouse_cursor(mouse_arrow, Input.CURSOR_ARROW)
+	var hotspot = mouse_arrow.get_size() / 2
+	Input.set_custom_mouse_cursor(mouse_arrow, Input.CURSOR_ARROW, hotspot)
