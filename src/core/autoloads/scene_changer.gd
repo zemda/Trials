3b661d3fc11702e46822f1a4c69a23c343aa7673
Daniel Žemla <zemladan@fit.cvut.cz>
2025-03-16 12:55:39 +0100

    make loading screen singleton

--- src/core/autoloads/scene_changer.gd
+++ src/core/autoloads/scene_changer.gd
@@ -1,12 +1,10 @@
 extends Node
 
 signal scene_loaded
-signal progress_changed(value)
 
 @export var max_load_time = 10000 # 10 seconds max loading time
 
 var _is_changing_scene: bool = false
-var _loading_screen = null
 
 
 func _ready():
@@ -27,24 +25,21 @@ func goto_scene(path):
 		_is_changing_scene = false
 		return
 	
-	_create_loading_screen()
+	LoadingScreen.show_loading_screen()
 	GameManager.disable_player_input()
 	var current_scene = get_tree().current_scene
-	
 	var t = Time.get_ticks_msec()
 	while Time.get_ticks_msec() - t < max_load_time:
-		var status = ResourceLoader.load_threaded_get_status(path)
+		var progress_array = []
+		var status = ResourceLoader.load_threaded_get_status(path, progress_array)
 		match status:
 			0, 2: # THREAD_LOAD_INVALID_RESOURCE, THREAD_LOAD_FAILED
-				print("SceneChanger: Error while loading file")
-				_clean_loading_screen()
+				LoadingScreen.hide_loading_screen()
 				_is_changing_scene = false
 				break
 			1: # THREAD_LOAD_IN_PROGRESS
-				var progress_array = []
-				ResourceLoader.load_threaded_get_status(path, progress_array)
 				var progress = progress_array[0] if progress_array.size() > 0 else 0.0
-				emit_signal("progress_changed", progress)
+				LoadingScreen.update_progress(progress)
 			3: # THREAD_LOAD_LOADED
 				var resource = ResourceLoader.load_threaded_get(path)
 				var new_scene = resource.instantiate()
@@ -56,8 +51,8 @@ func goto_scene(path):
 				if current_scene:
 					current_scene.queue_free()
 				
-				_clean_loading_screen()
 				GameManager.enable_player_input()
+				LoadingScreen.hide_loading_screen()
 				
 				_is_changing_scene = false
 				emit_signal("scene_loaded")
@@ -66,29 +61,5 @@ func goto_scene(path):
 		await get_tree().process_frame
 	
 	if _is_changing_scene:
-		print("SceneChanger: timedout")
-		_clean_loading_screen()
+		LoadingScreen.hide_loading_screen()
 		_is_changing_scene = false
-
-
-func _create_loading_screen() -> void:
-	var existing_screens = get_tree().get_nodes_in_group("loading_screen")
-	for screen in existing_screens:
-		if is_instance_valid(screen):
-			screen.queue_free()
-	
-	var _loading = SceneManager.get_loading_screen_instance()
-	if _loading:
-		get_tree().root.call_deferred("add_child", _loading)
-		_loading_screen = _loading
-		
-		await get_tree().process_frame
-		_loading_screen.show_loading_screen()
-
-
-func _clean_loading_screen() -> void:
-	if _loading_screen and is_instance_valid(_loading_screen):
-		_loading_screen.hide_loading_screen()
-		await get_tree().create_timer(0.3).timeout
-		_loading_screen.queue_free()
-		_loading_screen = null
--- src/ui/loading_screen/loading_screen.gd
+++ src/ui/loading_screen/loading_screen.gd
@@ -5,32 +5,17 @@ extends CanvasLayer
 @onready var _control_node := $Control
 @onready var _panel := $Control/Panel
 
-var _is_ready: bool = false
-
 
 func _ready():
 	add_to_group("loading_screen")
 	process_mode = Node.PROCESS_MODE_ALWAYS
-	
-	_is_ready = true
-	
-	if _control_node.visible == true:
-		_progress_bar.value = 0
-		_loading_text.text = "LOADING..."
-	else:
-		_control_node.visible = false
-	
-	if SceneChanger.has_signal("progress_changed"):
-		SceneChanger.connect("progress_changed", Callable(self, "update_progress"))
+	visible = false
 
 
 func show_loading_screen():
-	if !_is_ready:
-		return
-	
+	visible = true
 	layer = 128
 	
-	_control_node.visible = true
 	_control_node.modulate.a = 1.0
 	_progress_bar.value = 0
 	
@@ -38,20 +23,13 @@ func show_loading_screen():
 
 
 func hide_loading_screen():
-	if !_is_ready:
-		return
-	
+	visible = false
 	var tween = create_tween()
 	tween.tween_property(_control_node, "modulate:a", 0.0, 0.3)
 	await tween.finished
-	
-	_control_node.visible = false
 
 
 func update_progress(value: float):
-	if !_is_ready:
-		return
-		
 	var percent = value
 	if value <= 1.0:
 		percent = value * 100
