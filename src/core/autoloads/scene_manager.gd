798d53c20b2cd93cef06026804bd2ce27429c705
zemda <dankilom26@gmail.com>
2025-03-21 21:20:22 +0100

    update levels

--- src/core/autoloads/scene_manager.gd
+++ src/core/autoloads/scene_manager.gd
@@ -3,7 +3,7 @@ extends Node
 const LoadingScreenPath = "res://ui/loading_screen/LoadingScreen.tscn"
 const PauseScreenPath = "res://ui/pause_screen/PauseScreen.tscn"
 const PlayerPath = "res://nodes/player/player.tscn"
-const Level01Path= "res://levels/Level01.tscn"
+const Level01Path= "res://levels/level01/Level01.tscn"
 const StartScreenPath = "res://ui/start_screen/StartScreen.tscn"
 
 
